1c90363be0b08e38c2bda063079a13f5c6e8a62b
huycoijz <K205480106055@tnut.edu.vn>
2023-05-05 15:09:50 +0700

    5/5

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
